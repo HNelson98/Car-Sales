c1bbb4d0308979579f9357858a9819621f848658
Henry Nelson <Henry.Nelson89@gmail.com>
2020-04-15 07:02:53 -0700

    You can add features to your car now

--- src/reducers/CarReducer.js
+++ src/reducers/CarReducer.js
@@ -1,3 +1,5 @@
+import {ADD_FEATURE, REMOVE_FEATURE} from '../actions/carActions'
+
 export const initialState =
     {
         additionalPrice: 0,
@@ -21,6 +23,32 @@ export const carReducer = (state = initialState, action) => {
     console.log(state, action)
 
     switch (action.type){
+
+        case ADD_FEATURE:
+            return {
+                ...state,
+                additionalPrice: state.additionalPrice + action.payload.price,
+                car: {
+                  ...state.car,
+                  features: [
+                    ...state.car.features,
+                    action.payload
+                  ]
+                }
+            }
+        
+        case REMOVE_FEATURE:
+          return {
+            ...state,
+            additionalPrice: state.additionalPrice - action.payload.price,
+            car: {
+              ...state.car,
+              features: [
+                state.car.features.filter(action => action.payload)
+              ]
+            }
+          }
+
     default: 
     return state
     }
